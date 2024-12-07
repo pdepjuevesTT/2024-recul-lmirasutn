e0342c79d15c2b197564874c51e47e8e04afd7b8
lmirasutn <lmiras@frba.utn.edu.ar>
2024-12-07 10:35:08 -0300

    Update solucion.pl

--- solucion.pl
+++ solucion.pl
@@ -1,10 +1,30 @@
-% Solución en Prolog
 %vive(Persona, Vivienda, Localidad)
 %casa(metros)
-vive(juan,casa(120),almagro).
-vive(nico,departamento(3,2),almagro). %departamento(ambientes,baños)
-vive(alf,departamento(3,1),almagro).
-vive(julian,loft(2000),almagro). %loft(anio)
-vive(vale,departamento(4,1),flores).
-vive(fer,casa(110),flores).
-seQuiereMudar(rocio,casa(90)).
+vive(juan,vivienda(casa,120,almagro)).
+vive(nico,vivienda(departamento,3,2,almagro)). %departamento(ambientes,baños)
+vive(alf,vivienda(departamento,3,1,almagro)).
+vive(julian,vivienda(loft,2000,almagro)). %loft(anio)
+vive(vale,vivienda(departamento,4,1,flores)).
+vive(fer,vivienda(casa,110,flores)).
+seQuiereMudar(rocio,vivienda(casa,90)).
+%% 2. esCopada
+
+esCopada(casa):-
+    vive(_,vivienda(casa,Metros,_)),
+    Metros > 100.
+
+esCopada(Vivienda):-
+    vive(_,vivienda(Vivienda,Ambientes,_,_)),
+    Ambientes > 3.
+
+esCopada(Vivienda):-
+    vive(_,vivienda(Vivienda,_,Banios,_)),
+    Banios > 1.
+
+esCopada(Vivienda):-
+    vive(_,vivienda(Vivienda,AnioConstruccion,_)),
+    AnioConstruccion > 2015.
+
+barrioCopado(Barrio):-
+    vive(_,vivienda(Vivienda,_,_)),
+    forall(vive(_,vivienda(Vivienda,_,Barrio), esCopada(Vivienda))).
