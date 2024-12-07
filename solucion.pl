bd87e3db526b116781ff2900983c87064a5dc8a9
lmirasutn <lmiras@frba.utn.edu.ar>
2024-12-07 09:39:48 -0300

    Update solucion.pl

--- solucion.pl
+++ solucion.pl
@@ -1,1 +1,10 @@
 % Solución en Prolog
+%vive(Persona, Vivienda, Localidad)
+%casa(metros)
+vive(juan,casa(120),almagro).
+vive(nico,departamento(3,2),almagro). %departamento(ambientes,baños)
+vive(alf,departamento(3,1),almagro).
+vive(julian,loft(2000),almagro). %loft(anio)
+vive(vale,departamento(4,1),flores).
+vive(fer,casa(110),flores).
+seQuiereMudar(rocio,casa(90)).
